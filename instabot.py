742c10982a34c2dc694c44fb675fe23d420c3943
@abhishekkumar95544 <abhi95544@gmail.com>
2017-07-04 10:41:14 +0530

    requesting the url

--- instabot.py
+++ instabot.py
@@ -58,6 +58,7 @@ def get_user_info(insta_username):
    #printuser does not exist
         print 'User does not exist!'
         exit()
+#requesting the url
     request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     user_info = requests.get(request_url).json()
