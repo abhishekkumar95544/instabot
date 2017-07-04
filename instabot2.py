5dcf64c97a1fd8c28da3cc3018f6ae930d767278
@abhishekkumar95544 <abhi95544@gmail.com>
2017-07-04 12:14:50 +0530

    user media

--- instabot2.py
+++ instabot2.py
@@ -112,7 +112,7 @@ def get_user_post(insta_username):
     request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     user_media = requests.get(request_url).json()
-
+#user media
     if user_media['meta']['code'] == 200:
         if len(user_media['data']):
             image_name = user_media['data'][0]['id'] + '.jpeg'
