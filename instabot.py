b750efa71fc75f61f549ffb381858433aedacf6b
@abhishekkumar95544 <abhi95544@gmail.com>
2017-07-04 10:43:00 +0530

    printing it

--- instabot.py
+++ instabot.py
@@ -70,6 +70,7 @@ def get_user_info(insta_username):
             print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
             print 'No. of posts: %s' % (user_info['data']['counts']['media'])
         else:
+ #printing it 
             print 'There is no data for this user!'
     else:
         print 'Status code other than 200 received!'
