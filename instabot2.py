03dac581b2e7cb28cd0b6a9227a50820d7c67184
@abhishekkumar95544 <abhi95544@gmail.com>
2017-07-04 12:27:17 +0530

    in case of own media

--- instabot2.py
+++ instabot2.py
@@ -87,7 +87,7 @@ def get_own_post():
     request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     own_media = requests.get(request_url).json()
-
+#in case of own media
     if own_media['meta']['code'] == 200:
         if len(own_media['data']):
             image_name = own_media['data'][0]['id'] + '.jpeg'
