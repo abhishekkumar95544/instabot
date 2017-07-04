12e3f860eb39d425c9469341d31d8783987dc471
@abhishekkumar95544 <abhi95544@gmail.com>
2017-07-04 12:25:59 +0530

    printing the status of code

--- instabot2.py
+++ instabot2.py
@@ -74,6 +74,7 @@ def get_user_info(insta_username):
             #if yes then print the data for user
             print 'There is no data for this user!'
     else:
+        #printing the status of code
         print 'Status code other than 200 received!'
 
 
