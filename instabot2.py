8552908c66225b0dfce763f2bea2df12310944f3
@abhishekkumar95544 <abhi95544@gmail.com>
2017-07-05 10:00:05 +0530

    printing the status of code

--- instabot2.py
+++ instabot2.py
@@ -46,6 +46,7 @@ def get_user_id(insta_username):
         else:
             return None
     else:
+        #printing the status of the code
         print 'Status code other than 200 received!'
         exit()
 
